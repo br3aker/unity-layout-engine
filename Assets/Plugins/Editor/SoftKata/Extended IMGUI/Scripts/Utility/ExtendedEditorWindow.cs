9e30d8e7a4d5980ea0e56fb32222df739ce86f7b
Pentin Dmitry <riotbr3aker@gmail.com>
2020-04-19 17:06:03 +0300

    Uncommented release version code in ExtendedEditorWindow class

--- Assets/Plugins/Editor/SoftKata/Extended IMGUI/Scripts/Utility/ExtendedEditorWindow.cs
+++ Assets/Plugins/Editor/SoftKata/Extended IMGUI/Scripts/Utility/ExtendedEditorWindow.cs
@@ -6,24 +6,20 @@ namespace SoftKata.ExtendedEditorGUI {
     public abstract class ExtendedEditorWindow : EditorWindow {
         private ScrollGroup _rootScrollGroup;
 
-        private void OnEnable(){
+        private void OnEnable() {
             _rootScrollGroup = new ScrollGroup(Vector2.zero, Vector2.zero, false, ExtendedEditorGUI.LayoutResources.ScrollGroup);
             InitEditorWindow(this);
             Initialize();
         }
 
-        private void OnFocus() {
-            InitEditorWindow(this);
-        }
-
         public void OnGUI() {
             if (Event.current.type == EventType.Used) return;
 
-            // _rootScrollGroup.ContainerSize = new Vector2(Layout.AutoWidth, position.size.y);
-            // if (Layout.BeginLayoutGroup(_rootScrollGroup)) {
+            _rootScrollGroup.ContainerSize = new Vector2(Layout.AutoWidth, position.size.y);
+            if (Layout.BeginLayoutGroup(_rootScrollGroup)) {
                 IMGUI();
-            // }
-            // Layout.EndLayoutGroup();
+            }
+            Layout.EndLayoutGroup();
         }
 
         protected abstract void Initialize();
