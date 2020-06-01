a79cb8e26bfc1604effb6053b591be71633fbf0d
Pentin Dmitry <riotbr3aker@gmail.com>
2020-06-01 20:46:13 +0300

    Added completionist tag to the ScrollGroup

--- Assets/Plugins/Editor/SoftKata/Extended IMGUI/Scripts/Layout/Vertical groups/ScrollGroup.cs
+++ Assets/Plugins/Editor/SoftKata/Extended IMGUI/Scripts/Layout/Vertical groups/ScrollGroup.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.Events;
 
 namespace SoftKata.UnityEditor {
+    // TODO: code review done
     public class ScrollGroup : VerticalGroup {
         private const float _minimalScrollBarSize = 35;
 
